e98a1be24d556e2c53b2a77154d0138799bc3db9
Cesur APAYDIN <apaydin541@gmail.com>
2024-03-30 23:41:02 +0300

    Fix TS Extractor

--- src/Thor/Template/typescript/resource.ts.php
+++ src/Thor/Template/typescript/resource.ts.php
@@ -2,7 +2,7 @@
 <?php if (count($resources)) {
     echo PHP_EOL;
 } ?>
-<?php echo implode("\n", array_map(fn ($v) => sprintf("import { %s } from '../Resource/%s';", $v, $v), $resources)); ?>
+<?php echo implode("\n", array_map(fn ($v) => sprintf("import type { %s } from '../Resource/%s';", $v, $v), $resources)); ?>
 <?php if (count($resources)) {
     echo PHP_EOL;
 } ?>
